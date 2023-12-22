2864d9b0deae3cb40f4f66d16ef1f6979a073f33
krispikris <kris@kristopherhan.com>
2023-12-21 21:34:07 -0800

    testing

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import streamlit as st
 import pandas as pd
+
+st.write(
+    """
+    # My first app
+    Hello *world!*     
+    
+    """
+)
+
+st.chat_input("Message krisGPT...... testing testing")
