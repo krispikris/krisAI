91ffcc24f240f33828b8a72c72e6e08b387c2ead
krispikris <kris@kristopherhan.com>
2023-12-21 22:31:25 -0800

    random

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
 import streamlit as st
-import pandas as pd
+import random
+import time
 
-st.write(
-    """
-    # My first app
-    Hello *world!*     
-    """
-)
+st.title("Simple chat")
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = random.choice(
+            [
+                "a",
+                "b",
+                "c",
+            ]
+        )
+        # print("RANDOM ARRAY?: ", random)
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
